4894e0c258f7e8a09902028b0c53e91992a721d9
Boyd H. Ako <Boyd.Hanalei.Ako@gmail.com>
2020-07-21 00:51:37 -1000

    updating set_net_loc.bash

--- Network_location/set_net_loc.bash
+++ Network_location/set_net_loc.bash
@@ -2,11 +2,14 @@
 locfile="$(dirname $0)/mac_net.txt"
 defaultloc="Public"
 
-for gwmac in $(netstat -rn | sed -e 's~%~ ~g' | awk '$1 == "default" {printf $2"\n"}' | sort | uniq); do
+for gwmac in $(netstat -rn | sed -e 's~%~ ~g' | awk '$1 == "default" {printf $2"\n"}' | sort | uniq | egrep -v -e "\." -e "::$"); do
 	loc="$(awk -v gwmac="$gwmac" '$1 == gwmac {printf $2}' $locfile)"
-	if [ "$(awk -v loc="$loc" '$2 == loc {printf $0"\n"}' $locfile | wc -l)" -gt "0" ]; then
-		scselect $loc
-	else
-		scselect $defaultloc
+	curloc="$(scselect | awk '$1 == "*" {printf $NF"\n"}' | sed -e 's~(~~g' -e 's~)~~g')"
+	if [ "$curloc" != "$loc" ]; then
+		if [ "$(awk -v loc="$loc" '$2 == loc {printf $0"\n"}' $locfile | wc -l)" -gt "0" ]; then
+			scselect $loc
+		else
+			scselect $defaultloc
+		fi
 	fi
 done
