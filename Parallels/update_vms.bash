c12b612484e0f05c6fddecdc46e9ceaab1d66c0a
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-03-02 03:01:00 -1000

    Cleaned up output

--- Parallels/update_vms.bash
+++ Parallels/update_vms.bash
@@ -3,7 +3,7 @@
 function STARTVMS {
     for uuid in $(prlctl list -a | awk '{print $1}' | sed -e 's/{//g' -e 's/}//g' ); do
         if [ "$uuid" != "UUID" ]; then
-            state="$(prlctl list --info $uuid | awk '$1 == "State:" {print $2}')"
+            state="$(prlctl list --info "$uuid" | awk '$1 == "State:" {print $2}')"
             if [ "$state" != "running" ]; then
                 prlctl start $uuid &
             elif [ "$state" == "paused" ]; then
@@ -15,37 +15,39 @@ function STARTVMS {
 }
 function DETECTOS {
     for uuid in $(prlctl list -a | awk '{print $1}' | sed -e 's/{//g' -e 's/}//g' ); do
-        prlctl exec $uuid "uname -a" > /dev/null 2>&1
-        if [ "$(echo $?)" == "0" ]; then
-            os=linux
-        fi
-        prlctl exec $uuid "systeminfo" > /dev/null 2>&1
-        if [ "$(echo $?)" == "0" ]; then
-            os=windows
-        fi
-        if [ "$os" == "linux" ]; then
-            id="$(prlctl exec $uuid "cat /etc/os-release" | awk -F= '$1 == "ID" {print $2}')"
-            id_like="$(prlctl exec $uuid "cat /etc/os-release" | awk -F= '$1 == "ID_LIKE" {print $2}')"
-            
-            if [ ! -z "$id_like" ]; then
-                echo $id_like
-            else
-                case $id in
+        if [ "$uuid" != "UUID" ]; then
+            vmname="$(prlctl list --all | awk -v uuid="\{$uuid\}" '$1 == uuid {print $NF}')"
+            prlctl exec $uuid "uname -a" > /dev/null 2>&1
+            if [ "$(echo $?)" == "0" ]; then
+                os=linux
+            fi
+            prlctl exec $uuid "systeminfo" > /dev/null 2>&1
+            if [ "$(echo $?)" == "0" ]; then
+                os=windows
+            fi
+            if [ "$os" == "linux" ]; then
+                id="$(prlctl exec $uuid "cat /etc/os-release" | awk -F= '$1 == "ID" {print $2}')"
+                id_like="$(prlctl exec $uuid "cat /etc/os-release" | awk -F= '$1 == "ID_LIKE" {print $2}' | sed -e 's/"//g' -e 's/ /_/g')"
+                
+                case "$id_like" in
                     fedora)
                         id_like=redhat
                         ;;
+                    rhel_fedora)
+                        id_like=redhat
+                        ;;
                 esac
+                printf "\n=====\nVM Name:\t%s\nVM UUID:\t%s\nOS ID:\t%s\n-----\n" "$vmname" "$uuid" "$id_like"
+            elif [ "$os" ==  "windows" ]; then
+                id_like="$(prlctl exec $uuid "systeminfo" | awk -F: '$1 == "OS Name" {print $NF}' | sed -e 's/  //g' -e 's/^ //g' | sed 's/ /_/g')"
+            elif [ -z "$os" ]; then
+                printf "\n\nVM os undetected:\n%s\n\n\n" "$(prlctl list info $uuid)"
             fi
-            printf "\n\n%s : %s\n\n\n" "$uuid" "$id_like"
-        elif [ "$os" ==  "windows" ]; then
-            prlctl exec $uuid "systeminfo" | awk -F: '$1 == "OS Name" {print $NF}' | sed 's/ /_/g'
-        elif [ -z "$os" ]; then
-            printf "\n\nVM os undetected:\n%s\n\n\n" "$(prlctl list info $uuid)"
+            OSUPDATE 
+            unset os
+            unset id
+            unset id_like
         fi
-        OSUPDATE 
-        unset os
-        unset id
-        unset id_like
     done
 }
 function OSUPDATE {
@@ -58,6 +60,9 @@ function OSUPDATE {
             prlctl snapshot $uuid --description "Scripted update $(date)"
             prlctl exec $uuid "apt clean all && apt update -y && apt full-upgrade -y && apt auto-remove -y"
             ;;
+        *Windows*)
+            printf "System is %s.\n" "$id_like"
+            ;;
     esac
     if [ "$os" == "linux" ]; then
         prlctl exec $uuid "shutdown now"
@@ -67,4 +72,5 @@ function UPDATEVMS {
     STARTVMS
     DETECTOS
 }
+
 UPDATEVMS
