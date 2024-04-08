e036f0bb4bfecf4657e270444a5e4c6ffa049b39
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-04-07 15:30:01 -1000

    adjusted to remove all snapshots

--- Parallels/clear_snapshots.bash
+++ Parallels/clear_snapshots.bash
@@ -1,7 +1,9 @@
 #!/bin/bash -xv
 
 for uuid in $(prlctl list --all | awk '$1 != "UUID" {print $1}' | sed -e 's/{//g' -e 's/}//g'); do
-    for snapshotid in $(prlctl snapshot-list $uuid | awk '$2 != "SNAPSHOT_ID" {print $2}' | sed -e 's/^.*{//g' -e 's/}//g'); do
+    for snapshotid in $(prlctl snapshot-list $uuid | awk '$2 != "SNAPSHOT_ID" {print $NF}' | sed -e 's/^.*{//g' -e 's/}//g'); do
         prlctl snapshot-delete $uuid --id $snapshotid --children
+        prlctl snapshot-delete $uuid --id $snapshotid
+
     done
 done
