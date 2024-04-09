5358635fde2f36a4d69ff36a63d5e4761bd13f0a
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-04-09 00:20:47 -1000

    fixed Windows id_like variable

--- Parallels/update_vms.bash
+++ Parallels/update_vms.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 function STARTVMS {
     for uuid in $(prlctl list -a | awk '{print $1}' | sed -e 's/{//g' -e 's/}//g' ); do
@@ -40,9 +40,9 @@ function DETECTOS {
                         id_like=redhat
                         ;;
                 esac
-                printf "\n=====\nVM Name:\t%s\nVM UUID:\t%s\nOS ID:\t%s\n-----\n" "$vmname" "$uuid" "$id_like"
+                printf "\n=====\nVM Name:%s\nVM UUID:%s\nOS ID:%s\n-----\n" "$vmname" "$uuid" "$id_like"
             elif [ "$os" ==  "windows" ]; then
-                id_like="$(prlctl exec $uuid "systeminfo" | awk -F: '$1 == "OS Name" {print $NF}' | sed -e 's/  //g' -e 's/^ //g' | sed 's/ /_/g')"
+                id_like="$(prlctl exec $uuid "systeminfo" | dos2unix | awk -F: '$1 == "OS Name" {print $NF}' | sed -e 's/  //g' -e 's/^ //g' -e 's/ /_/g' | tr -d [[:blank:]])"
             elif [ -z "$os" ]; then
                 printf "\n\nVM os undetected:\n%s\n\n\n" "$(prlctl list info $uuid)"
             fi
@@ -64,11 +64,11 @@ function OSUPDATE {
             prlctl exec $uuid "apt clean all && apt update -y && apt full-upgrade -y && apt auto-remove -y && touch /.autorelabel"
             ;;
         *Windows*)
-            printf "System is %s.\n" "$id_like"
+            printf "System is %s.\n" "$( echo $id_like | sed 's/_/ /g')"
             ;;
     esac
     if [ "$os" == "linux" ]; then
-        prlctl exec $uuid "shutdown now"
+        prlctl exec $uuid "shutdown -r now"
     fi
 }
 function UPDATEVMS {
